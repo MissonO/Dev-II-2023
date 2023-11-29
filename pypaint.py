56fd131a42b33fa136b2835dfd50891cb995a179
Oscar <oscar.misson@gmail.com>
2023-11-29 11:20:56 +0100

    brush size addition

--- pypaint.py
+++ pypaint.py
@@ -1,11 +1,12 @@
 import tkinter as tk
-from tkinter import colorchooser
+from tkinter import colorchooser, simpledialog
 
 
 class Brush:
-    def __init__(self, canvas, color="black"):
+    def __init__(self, canvas, color="black", size=2):
         self.canvas = canvas
         self.color = color
+        self.size = size
 
     def draw(self, x, y):
         raise NotImplementedError()
@@ -13,15 +14,18 @@ class Brush:
     def set_color(self, color):
         self.color = color
 
+    def set_size(self, size):
+        self.size = size
+
 
 class PencilBrush(Brush):
     def draw(self, x, y):
         self.set_color(self.canvas.color)
         self.canvas.create_oval(
-            x - 2,
-            y - 2,
-            x + 2,
-            y + 2,
+            x - self.size,
+            y - self.size,
+            x + self.size,
+            y + self.size,
             fill=self.color,
             outline=self.color
         )
@@ -29,11 +33,11 @@ class PencilBrush(Brush):
 
 class EraserBrush(Brush):
     def draw(self, x, y):
-        self.canvas.create_rectangle(
-            x - 5,
-            y - 5,
-            x + 5,
-            y + 5,
+        self.canvas.create_oval(
+            x - self.size,
+            y - self.size,
+            x + self.size,
+            y + self.size,
             fill="white",
             outline="white"
         )
@@ -46,6 +50,7 @@ class Canvas(tk.Canvas):
         self.bind("<B1-Motion>", self.draw)
         self.color = "black"
         self.eraser_mode = False
+        self.brush_size = 2
 
     # Definit la fonction de dessin
     def draw(self, event):
@@ -54,9 +59,9 @@ class Canvas(tk.Canvas):
         x, y = event.x, event.y
 
         if self.eraser_mode:
-            self.brush = EraserBrush(self)
+            self.brush = EraserBrush(self, size=self.brush_size)
         else:
-            self.brush = PencilBrush(self)
+            self.brush = PencilBrush(self, size=self.brush_size)
 
         self.brush.draw(x, y)
 
@@ -69,26 +74,11 @@ class Canvas(tk.Canvas):
     def toggle_brush_mode(self):
         self.eraser_mode = False
 
+    def set_brush_size(self, size):
+        self.brush_size = size
 
-class BrushButton(tk.Button):
-    def __init__(self, master, canvas, **kwargs):
-        super().__init__(
-            master,
-            text="Pinceau",
-            command=self.toggle_brush_mode,
-            **kwargs
-        )
-        self.canvas = canvas
-        self.selected_color = "lightgrey"
-        self.unselected_color = "SystemButtonFace"
-        self.configure(bg=self.unselected_color)
-
-    def toggle_brush_mode(self):
-        self.canvas.toggle_brush_mode()
-        if self.canvas.eraser_mode:
-            self.configure(bg=self.unselected_color)
-        else:
-            self.configure(bg=self.selected_color)
+    def update_brush_size_label(self):
+        brush_size_label.config(text=f"Brush Size: {self.brush_size}")
 
 
 class EraserButton(tk.Button):
@@ -112,6 +102,27 @@ class EraserButton(tk.Button):
             self.configure(bg=self.unselected_color)
 
 
+class SizeButton(tk.Button):
+    def __init__(self, master, canvas, **kwargs):
+        super().__init__(
+            master,
+            text="Taille",
+            command=self.choose_size,
+            **kwargs
+        )
+        self.canvas = canvas
+
+    def choose_size(self):
+        size = tk.simpledialog.askinteger(
+            "Brush Size",
+            "Enter brush size:",
+            initialvalue=self.canvas.brush_size
+        )
+        if size is not None:
+            self.canvas.brush_size = size
+            self.canvas.update_brush_size_label()
+
+
 class DrawingApp:
     def __init__(self, root):
         self.root = root
@@ -128,10 +139,16 @@ class DrawingApp:
         )
         color_button.pack(side=tk.RIGHT)
 
-        brush_button = BrushButton(root, self.canvas)
-        brush_button.pack(side=tk.LEFT)
         eraser_button = EraserButton(root, self.canvas)
         eraser_button.pack(side=tk.LEFT)
+        size_button = SizeButton(root, self.canvas)
+        size_button.pack(side=tk.LEFT)
+        global brush_size_label
+        brush_size_label = tk.Label(
+            root,
+            text=f"Brush Size: {self.canvas.brush_size}"
+        )
+        brush_size_label.pack(side=tk.LEFT)
 
     def choose_color(self):
         color = colorchooser.askcolor()[1]
