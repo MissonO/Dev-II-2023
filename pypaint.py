1de5b821f7793a45311e9fafb6a9c0808b03b108
Oscar <oscar.misson@gmail.com>
2024-08-13 15:32:12 +0200

    ArgAddition

--- pypaint.py
+++ pypaint.py
@@ -5,10 +5,10 @@ from drawing_app import DrawingApp
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Application de dessin avec Tkinter")
-    parser.add_argument('--background', type=str,
+        description="Application de dessin")
+    parser.add_argument('-bg', '--background', nargs='?', type=str,
                         default="white",
-                        help="Couleur de fond du document")
+                        help="Couleur de fond")
     return parser.parse_args()
 
 
